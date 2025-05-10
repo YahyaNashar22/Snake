51d165b225095f5a57cf455833d46d1679cc0b66
YahyaNashar22 <yahyanashar22@gmail.com>
2025-05-10 09:22:21 +0300

    Created enum to represent game object annotations

---
+++ Snake/GridValue.cs
@@ -0,0 +1,11 @@
+﻿namespace Snake
+{
+    // Represents the values in the grid to determine different aspects of the game objects
+   public enum GridValue
+    {
+        Empty,
+        Snake,
+        Food,
+        Outside
+    }
+}
